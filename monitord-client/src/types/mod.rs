0ba3bdb47a5784a12b2a53720649726bdbeb23e3
Adam Cosner <adam.cosner@proton.me>
2025-03-16 05:39:26 +0000

    monitord_client: scrapped old structure because it was dumb and I have a better idea

--- monitord-client/src/types/mod.rs
+++
@@ -1,8 +0,0 @@
-//! Type definitions for the monitord client
-//!
-//! This module re-exports the protocol types from the monitord-protocols crate
-//! with additional client-specific functionality.
-
-pub use monitord_protocols::monitord::{
-    CpuInfo, GpuInfo, MemoryInfo, NetworkInfo, ProcessInfo, StorageInfo, SystemInfo, SystemSnapshot,
-};
