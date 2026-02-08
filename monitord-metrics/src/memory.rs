36c38931b8e1346a58a9b115f5897d08f4811fcf
Adam Cosner <adam.cosner@proton.me>
2026-02-08 13:09:29 -0500

    Added optimization to not retry udevadm every call to collect if it fails

--- monitord-metrics/src/memory.rs
+++ monitord-metrics/src/memory.rs
@@ -17,18 +17,16 @@ pub use crate::metrics::Memory as Snapshot;
 
 /// The metric collector, create an instance with `memory::Collector::new()` and collect with `collector.collect()`
 pub struct Collector {
-    speed: Option<u64>,
-    form_factor: Option<String>,
-    ram_type: Option<String>,
+    sp_rt_ff: Option<(u64, String, String)>,
+    tried_udevadm: bool,
 }
 
 impl Collector {
     /// Create a new instance of the collector
     pub fn new() -> Self {
         Self {
-            speed: None,
-            form_factor: None,
-            ram_type: None,
+            sp_rt_ff: None,
+            tried_udevadm: false,
         }
     }
 
@@ -43,52 +41,13 @@ impl Collector {
         let available = meminfo.mem_available.unwrap_or(0);
         let swap_capacity = meminfo.swap_total;
         let swap_in_use = meminfo.swap_total - meminfo.swap_free;
-        let speed = match self.speed {
-            Some(speed) => speed,
+
+        let (speed, form_factor, ram_type) = match self.sp_rt_ff.clone() {
+            Some((speed, form_factor, ram_type)) => (speed, form_factor, ram_type),
             None => {
-                let mut cmd = Command::new("udevadm");
-                cmd.arg("info").arg("-q").arg("property").arg("-p").arg("/sys/devices/virtual/dmi/id");
-                cmd.env_remove("LD_PRELOAD");
-
-                let output = cmd.output().with_context(|| format!("{} on {}", file!(), line!()))?;
-                let stdout = String::from_utf8(output.stdout).with_context(|| format!("{} on {}", file!(), line!()))?;
-
-                let mut speed = 0;
-                let mut form_factor = String::new();
-                let mut ram_type = String::new();
-                let lines = stdout.lines().collect::<Vec<&str>>();
-                let slot_number = lines.iter().find(|line| line.starts_with("MEMORY_ARRAY_NUM_DEVICES")).map(|line| line.split('=').nth(1).unwrap().parse::<u32>().unwrap_or(0));
-                for slot_index in 0..slot_number.unwrap_or(0) {
-                    if let Some(speed_mts) = lines.iter().find(|line| line.starts_with(format!("MEMORY_DEVICE_{}_SPEED_MTS=", slot_index).as_str())).map(|line| line.split('=').nth(1).unwrap().parse::<u64>().unwrap_or(0)) {
-                        speed = core::cmp::max(speed, speed_mts);
-                    }
-                }
-                for slot_index in 0..slot_number.unwrap_or(0) {
-                    if let Some(form_factor_value) = lines.iter().find(|line| line.starts_with(format!("MEMORY_DEVICE_{}_FORM_FACTOR=", slot_index).as_str())).map(|line| line.split('=').nth(1).unwrap()) {
-                        if form_factor_value == "Unknown" {
-                            continue;
-                        }
-                        form_factor = form_factor_value.to_string();
-                        break;
-                    }
-                }
-                for slot_index in 0..slot_number.unwrap_or(0) {
-                    if let Some(ram_type_value) = lines.iter().find(|line| line.starts_with(format!("MEMORY_DEVICE_{}_TYPE=", slot_index).as_str())).map(|line| line.split('=').nth(1).unwrap()) {
-                        if ram_type_value == "Unknown" {
-                            continue;
-                        }
-                        ram_type = ram_type_value.to_string();
-                        break;
-                    }
-                }
-                self.ram_type = Some(ram_type.clone());
-                self.speed = Some(speed);
-                self.form_factor = Some(form_factor.clone());
-                speed
+                self.collect_from_udevadm().unwrap_or_default()
             }
         };
-        let form_factor = self.form_factor.clone().unwrap_or_default();
-        let ram_type = self.ram_type.clone().unwrap_or_default();
 
         Ok(Snapshot {
             capacity,
@@ -103,7 +62,60 @@ impl Collector {
             ram_type,
         })
     }
+
+    fn collect_from_udevadm(&mut self) -> anyhow::Result<(u64, String, String)> {
+        let mut cmd = Command::new("udevadm");
+        cmd.arg("info").arg("-q").arg("property").arg("-p").arg("/sys/devices/virtual/dmi/id");
+        cmd.env_remove("LD_PRELOAD");
+
+        self.tried_udevadm = true;
+
+        let output = cmd.output().with_context(|| format!("{} on {}", file!(), line!()))?;
+        let stdout = String::from_utf8(output.stdout).with_context(|| format!("{} on {}", file!(), line!()))?;
+
+        let mut speed = 0;
+        let mut form_factor = String::new();
+        let mut ram_type = String::new();
+        let lines = stdout.lines().collect::<Vec<&str>>();
+        let slot_number = lines.iter().find(|line| line.starts_with("MEMORY_ARRAY_NUM_DEVICES")).map(|line| line.split('=').nth(1).unwrap().parse::<u32>().unwrap_or(0));
+
+        // Speed
+        for slot_index in 0..slot_number.unwrap_or(0) {
+            if let Some(speed_mts) = lines.iter().find(|line| line.starts_with(format!("MEMORY_DEVICE_{}_SPEED_MTS=", slot_index).as_str())).map(|line| line.split('=').nth(1).unwrap().parse::<u64>().unwrap_or(0)) {
+                speed = core::cmp::max(speed, speed_mts);
+            }
+        }
+
+        // RAM Type
+        for slot_index in 0..slot_number.unwrap_or(0) {
+            if let Some(ram_type_value) = lines.iter().find(|line| line.starts_with(format!("MEMORY_DEVICE_{}_TYPE=", slot_index).as_str())).map(|line| line.split('=').nth(1).unwrap()) {
+                if ram_type_value == "Unknown" {
+                    continue;
+                }
+                ram_type = ram_type_value.to_string();
+                break;
+            }
+        }
+
+        // Form Factor
+        for slot_index in 0..slot_number.unwrap_or(0) {
+            if let Some(form_factor_value) = lines.iter().find(|line| line.starts_with(format!("MEMORY_DEVICE_{}_FORM_FACTOR=", slot_index).as_str())).map(|line| line.split('=').nth(1).unwrap()) {
+                if form_factor_value == "Unknown" {
+                    continue;
+                }
+
+                form_factor = form_factor_value.to_string();
+                break;
+            }
+        }
+
+        self.sp_rt_ff = Some((speed.clone(), ram_type.clone(), form_factor.clone()));
+
+        Ok((speed, ram_type, form_factor))
+    }
 }
+
+
 
 mod tests {
     #[test]
