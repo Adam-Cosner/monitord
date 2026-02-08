bb1246b29a335532d5639d321537c0475bc3a191
Adam Cosner <adam.cosner@proton.me>
2026-02-08 13:39:41 -0500

    Added MPL-2.0 license headers

--- monitord-metrics/src/cpu.rs
+++ monitord-metrics/src/cpu.rs
@@ -1,3 +1,9 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 /// CPU metric collection
 ///
 /// First sample will be empty due to needing two samples to calculate usage values
--- monitord-metrics/src/memory.rs
+++ monitord-metrics/src/memory.rs
@@ -1,4 +1,8 @@
-use std::process::Command;
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 
 /// Memory metric collection
 ///
@@ -11,6 +15,7 @@ use std::process::Command;
 /// ```
 use procfs::Current;
 use anyhow::Context;
+use std::process::Command;
 
 #[doc(inline)]
 pub use crate::metrics::Memory as Snapshot;
