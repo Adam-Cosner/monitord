36008f15f5a7e77ee3ef147995a8ec130cf0be44
Adam Cosner <adam.cosner@proton.me>
2026-06-03 19:05:15 -0400

    gpu/nouveau: get path of primary node and fd to render node

--- src/collector/gpu/nouveau.rs
+++ src/collector/gpu/nouveau.rs
@@ -4,15 +4,42 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-use std::os::fd::OwnedFd;
+use std::{os::fd::OwnedFd, path::PathBuf};
+
+use rustix::fs::{Mode, OFlags};
 
 pub struct Card {
-    // todo
+    primary_node: PathBuf,
+    render_node: OwnedFd,
 }
 
 impl Card {
     pub fn new(fd: OwnedFd) -> anyhow::Result<Self> {
-        Ok(Self {})
+        let drm_root = rustix::fs::openat(
+            &fd,
+            "device/drm",
+            rustix::fs::OFlags::DIRECTORY
+                | rustix::fs::OFlags::RDONLY
+                | rustix::fs::OFlags::CLOEXEC,
+            rustix::fs::Mode::empty(),
+        )?;
+        let mut primary_node = PathBuf::new();
+        let mut render_node = PathBuf::new();
+        for entry in rustix::fs::Dir::read_from(&drm_root)? {
+            let entry = entry?;
+            let name = entry.file_name().to_string_lossy().to_string();
+            if name.starts_with("card") {
+                primary_node = PathBuf::from(format!("/dev/dri/{}", name));
+            } else if name.starts_with("renderD") {
+                render_node = PathBuf::from(format!("/dev/dri/{}", name));
+            }
+        }
+        let render_node =
+            rustix::fs::open(render_node, OFlags::CLOEXEC | OFlags::RDWR, Mode::empty())?;
+        Ok(Self {
+            primary_node,
+            render_node,
+        })
     }
 }
 
@@ -30,6 +57,6 @@ impl super::Card for Card {
     }
 
     fn primary_node(&self) -> String {
-        todo!()
+        self.primary_node.to_string_lossy().to_string()
     }
 }
