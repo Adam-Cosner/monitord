082c179128f774fc7c4e49ab79d212af0374c898
Adam Cosner <adam.cosner@proton.me>
2026-06-11 20:21:01 -0400

    gpu/intel: implemented i915 and xe driver metric collection
    device names are pending

--- src/collector/gpu/i915.rs
+++ src/collector/gpu/i915.rs
@@ -4,11 +4,18 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-use std::{os::fd::OwnedFd, path::PathBuf};
+use crate::collector::helpers::{discovery::Discovery, sysfs};
+use crate::metrics::gpu::*;
+use std::path::PathBuf;
+
+use rustix::fd::{AsFd, OwnedFd};
 
 pub struct Card {
+    card_fd: OwnedFd,
     primary_node: PathBuf,
-    render_node: OwnedFd,
+    render_node: PathBuf,
+    render_node_fd: OwnedFd,
+    brand_name: Discovery<String>,
 }
 
 impl Card {
@@ -22,6 +29,7 @@ impl Card {
             rustix::fs::Mode::empty(),
         )?;
         let mut primary_node = PathBuf::new();
+        let mut render_node_path = PathBuf::new();
         let mut render_node = None;
         for entry in rustix::fs::Dir::read_from(&drm_subsystem)? {
             let entry = entry?;
@@ -29,6 +37,7 @@ impl Card {
             if name.starts_with("card") {
                 primary_node = PathBuf::from(format!("/dev/dri/{}", name));
             } else if name.starts_with("renderD") {
+                render_node_path = PathBuf::from(format!("/dev/dri/{}", name));
                 render_node = Some(rustix::fs::open(
                     format!("/dev/dri/{}", name),
                     rustix::fs::OFlags::RDWR
@@ -39,26 +48,249 @@ impl Card {
             }
         }
         Ok(Self {
+            card_fd: fd,
             primary_node,
-            render_node: render_node.ok_or_else(|| anyhow::anyhow!("render node not found"))?,
+            render_node: render_node_path,
+            render_node_fd: render_node.ok_or_else(|| anyhow::anyhow!("render node not found"))?,
+            brand_name: Discovery::default(),
         })
+    }
+
+    fn clocks(&self) -> Vec<Clock> {
+        let Some(current_frequency_mhz) =
+            sysfs::readat_u32(self.card_fd.as_fd(), "gt_cur_freq_mhz")
+        else {
+            return Vec::new();
+        };
+
+        let Some(max_frequency_mhz) = sysfs::readat_u32(self.card_fd.as_fd(), "gt_max_freq_mhz")
+        else {
+            return Vec::new();
+        };
+        vec![Clock {
+            identifier: Some(ClockIdentifier {
+                domain: ClockDomain::Gt as i32,
+                index: 0,
+            }),
+            current_frequency_mhz,
+            max_frequency_mhz,
+        }]
+    }
+
+    #[allow(unused_assignments)]
+    fn memory(&self) -> Vec<Memory> {
+        let mut query_item = drm_i915::QueryItem {
+            query_id: 4, // DRM_I915_QUERY_MEMORY_REGIONS
+            length: 0,   // will be filled in by ioctl
+            flags: 0,
+            data_ptr: 0, // must wait for length to be gotten before providing a data pointer to receive
+        };
+
+        let query = drm_i915::Query {
+            num_items: 1,
+            flags: 0,
+            items_ptr: &mut query_item as *mut _ as u64,
+        };
+        let Ok(query) = unsafe { rustix::ioctl::ioctl(self.render_node_fd.as_fd(), query) }
+            .inspect_err(|e| tracing::error!("failed to get i915 memory regions length: {e}"))
+        else {
+            return Vec::new();
+        };
+
+        // Now that query_item.length is populated, we can reserve a buffer
+        let mut bytes = Vec::new();
+        bytes.resize(query_item.length as usize, 0u8);
+        query_item.data_ptr = bytes.as_mut_ptr() as u64;
+        let Ok(_) = unsafe { rustix::ioctl::ioctl(self.render_node_fd.as_fd(), query) }
+            .inspect_err(|e| tracing::error!("failed to get i915 memory regions data: {e}"))
+        else {
+            return Vec::new();
+        };
+
+        // Now we transmute the bytes pointer to a QueryMemoryRegions struct
+        let Some(region_info) = core::ptr::NonNull::new(unsafe {
+            std::mem::transmute::<*const u8, *mut drm_i915::QueryMemoryRegions>(bytes.as_ptr())
+        })
+        .map(|ptr| unsafe { ptr.as_ref() }) else {
+            return Vec::new();
+        };
+
+        // Now we finally extract the MemoryRegionInfo values
+        let regions = unsafe {
+            region_info
+                .regions
+                .flex_ref(region_info.num_regions as usize)
+        };
+
+        regions
+            .iter()
+            .map(|region| Memory {
+                r#type: if region.memory_class == 0 {
+                    MemoryType::System as i32
+                } else {
+                    MemoryType::Vram as i32
+                },
+                total_memory: region.probed_size,
+                used_memory: region.probed_size - region.unallocated_size,
+            })
+            .collect()
+    }
+
+    fn power(&self) -> Option<Power> {
+        let Some(_hwmon_fd) = sysfs::first_hwmon_subdir_at(self.card_fd.as_fd(), "device/hwmon")
+        else {
+            return None;
+        };
+        None // I don't have an Arc GPU to figure out where the power file is located
+    }
+
+    fn thermals(&self) -> Vec<Thermal> {
+        let Some(_hwmon_fd) = sysfs::first_hwmon_subdir_at(self.card_fd.as_fd(), "device/hwmon")
+        else {
+            return Vec::new();
+        };
+        Vec::new() // Same as above
     }
 }
 
 impl super::Card for Card {
     fn collect(&mut self, config: &super::Config) -> anyhow::Result<super::Gpu> {
-        todo!()
+        let brand_name = self
+            .brand_name
+            .probe(|| Ok(String::from("todo")))
+            .cloned()
+            .unwrap_or_default();
+
+        let drivers = config
+            .drivers
+            .then(|| {
+                Some(Drivers {
+                    kernel: "i915".to_string(),
+                    ..Default::default()
+                })
+            })
+            .unwrap_or_default();
+
+        let engines = Vec::new();
+        let clocks = config.clocks.then(|| self.clocks()).unwrap_or_default();
+        let memory = config.memory.then(|| self.memory()).unwrap_or_default();
+        let power = config.power.then(|| self.power()).unwrap_or_default();
+        let thermals = config.thermals.then(|| self.thermals()).unwrap_or_default();
+
+        Ok(Gpu {
+            brand_name,
+            primary_node: self.primary_node.to_string_lossy().to_string(),
+            render_node: self.render_node.to_string_lossy().to_string(),
+            drivers,
+            engines,
+            clocks,
+            memory,
+            power,
+            thermals,
+            processes: Vec::new(),
+        })
     }
 
     fn resolve(
         &mut self,
-        staging: &crate::collector::staging::Staging,
-        output: super::Gpu,
+        _staging: &crate::collector::staging::Staging,
+        _output: super::Gpu,
     ) -> anyhow::Result<super::Gpu> {
         todo!()
     }
 
     fn primary_node(&self) -> String {
         self.primary_node.to_string_lossy().to_string()
+    }
+}
+
+mod drm_i915 {
+    use crate::collector::helpers::{ioctl::*, *};
+    use crate::{_ioc, _iowr};
+
+    /// Helper for a read-write DRM ioctl command
+    macro_rules! drm_iowr {
+        ($nr:expr, $ty:ty) => {
+            _iowr!(DRM_IOCTL_BASE, $nr, $ty)
+        };
+    }
+
+    /// The ioctl directory (drm)
+    const DRM_IOCTL_BASE: u32 = 'd' as u32;
+
+    /// The part at which driver-specific drm commands begin
+    const DRM_COMMAND_BASE: u32 = 0x40;
+
+    /// The offset of the query command
+    const DRM_I915_QUERY: u32 = 0x39;
+
+    /// The query command for drm ioctl
+    const DRM_IOCTL_I915_QUERY: u32 = drm_iowr!(DRM_COMMAND_BASE + DRM_I915_QUERY, Query);
+
+    /// struct drm_i915_query (linux/include/uapi/drm/i915_drm.h)
+    /// holds a list of query items
+    #[repr(C)]
+    #[derive(Debug, Clone)]
+    pub struct Query {
+        pub num_items: u32,
+        pub flags: u32,
+        pub items_ptr: u64,
+    }
+
+    /// struct drm_i915_query_item (linux/include/uapi/drm/i915_drm.h)
+    /// a single query to the i915 driver
+    #[repr(C)]
+    #[derive(Debug, Clone)]
+    pub struct QueryItem {
+        pub query_id: u64,
+        pub length: i32,
+        pub flags: u32,
+        pub data_ptr: u64,
+    }
+
+    unsafe impl rustix::ioctl::Ioctl for Query {
+        type Output = Self;
+        const IS_MUTATING: bool = true;
+
+        fn opcode(&self) -> rustix::ioctl::Opcode {
+            DRM_IOCTL_I915_QUERY
+        }
+
+        fn as_ptr(&mut self) -> *mut std::ffi::c_void {
+            self as *mut _ as *mut std::ffi::c_void
+        }
+
+        unsafe fn output_from_ptr(
+            _: rustix::ioctl::IoctlOutput,
+            extract_ptr: *mut std::ffi::c_void,
+        ) -> rustix::io::Result<Self::Output> {
+            let casted_ptr = core::ptr::NonNull::new(extract_ptr as *mut _ as *mut Self)
+                .map(|ptr| unsafe { ptr.as_ref() })
+                .ok_or(rustix::io::Errno::FAULT)?;
+
+            Ok(casted_ptr.clone())
+        }
+    }
+
+    /// struct drm_i915_query_memory_regions (linux/include/uapi/drm/i915_drm.h)
+    /// memory region list implemented as a flexible array member pattern
+    #[repr(C)]
+    pub struct QueryMemoryRegions {
+        pub num_regions: u32,
+        _rsvd: [u32; 3], // reserved
+        pub regions: fam::FAM<MemoryRegionInfo>,
+    }
+
+    /// Represents a memory region accessible to the GPU
+    #[repr(C)]
+    pub struct MemoryRegionInfo {
+        pub memory_class: u16,
+        pub memory_instance: u16,
+        _rsvd0: u32,
+        pub probed_size: u64,
+        pub unallocated_size: u64,
+        pub probed_cpu_visible_size: u64,
+        pub unallocated_cpu_visible_size: u64,
+        _rsvd1: [u64; 6],
     }
 }
--- src/collector/gpu/xe.rs
+++ src/collector/gpu/xe.rs
@@ -4,31 +4,339 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-use std::os::fd::OwnedFd;
+use crate::collector::helpers::{discovery::Discovery, sysfs};
+use crate::metrics::gpu::*;
+use std::path::PathBuf;
+
+use rustix::fd::{AsFd, OwnedFd};
+use rustix::fs::{AtFlags, Mode, OFlags};
 
 pub struct Card {
-    // todo
+    card_fd: OwnedFd,
+    primary_node: PathBuf,
+    render_node: PathBuf,
+    render_node_fd: OwnedFd,
+    brand_name: Discovery<String>,
 }
 
 impl Card {
     pub fn new(fd: OwnedFd) -> anyhow::Result<Self> {
-        Ok(Self {})
+        let drm_subsystem = rustix::fs::openat(
+            &fd,
+            "device/drm",
+            rustix::fs::OFlags::RDONLY
+                | rustix::fs::OFlags::DIRECTORY
+                | rustix::fs::OFlags::CLOEXEC,
+            rustix::fs::Mode::empty(),
+        )?;
+        let mut primary_node = PathBuf::new();
+        let mut render_node_path = PathBuf::new();
+        let mut render_node = None;
+        for entry in rustix::fs::Dir::read_from(&drm_subsystem)? {
+            let entry = entry?;
+            let name = entry.file_name().to_string_lossy().to_string();
+            if name.starts_with("card") {
+                primary_node = PathBuf::from(format!("/dev/dri/{}", name));
+            } else if name.starts_with("renderD") {
+                render_node_path = PathBuf::from(format!("/dev/dri/{}", name));
+                render_node = Some(rustix::fs::open(
+                    format!("/dev/dri/{}", name),
+                    rustix::fs::OFlags::RDWR
+                        | rustix::fs::OFlags::CLOEXEC
+                        | rustix::fs::OFlags::NONBLOCK,
+                    rustix::fs::Mode::empty(),
+                )?);
+            }
+        }
+        Ok(Self {
+            card_fd: fd,
+            primary_node,
+            render_node: render_node_path,
+            render_node_fd: render_node.ok_or_else(|| anyhow::anyhow!("render node not found"))?,
+            brand_name: Discovery::default(),
+        })
+    }
+
+    fn clocks(&self) -> Vec<Clock> {
+        let Ok(device_fd) = rustix::fs::openat(
+            self.card_fd.as_fd(),
+            "device",
+            OFlags::RDONLY | OFlags::CLOEXEC | OFlags::DIRECTORY,
+            Mode::empty(),
+        ) else {
+            return Vec::new();
+        };
+        let mut clocks = Vec::new();
+        // XE_MAX_TILES_PER_DEVICE
+        for i in 0..2 {
+            let Ok(tile_fd) = rustix::fs::openat(
+                device_fd.as_fd(),
+                format!("tile{}", i),
+                OFlags::RDONLY | OFlags::CLOEXEC | OFlags::DIRECTORY,
+                Mode::empty(),
+            ) else {
+                continue;
+            };
+
+            let Ok(dir) = rustix::fs::Dir::read_from(tile_fd.as_fd()) else {
+                continue;
+            };
+            for entry in dir {
+                let Ok(gt) = entry else {
+                    continue;
+                };
+
+                let Ok(gt_fd) = rustix::fs::openat(
+                    tile_fd.as_fd(),
+                    gt.file_name().to_string_lossy(),
+                    OFlags::RDONLY | OFlags::CLOEXEC | OFlags::DIRECTORY,
+                    Mode::empty(),
+                ) else {
+                    continue;
+                };
+
+                let mut is_graphics = false;
+                let mut is_video = false;
+
+                // Check if the render command streamer exists on this gt
+                if rustix::fs::statat(gt_fd.as_fd(), "engines/rcs", AtFlags::empty()).is_ok() {
+                    is_graphics = true;
+                }
+                // Check if the video command streamer exists
+                if rustix::fs::statat(gt_fd.as_fd(), "engines/vcs", AtFlags::empty()).is_ok() {
+                    is_video = true;
+                }
+
+                let Some(current_frequency_mhz) =
+                    sysfs::readat_u32(gt_fd.as_fd(), "engines/cur_freq")
+                else {
+                    continue;
+                };
+                let Some(max_frequency_mhz) = sysfs::readat_u32(gt_fd.as_fd(), "engines/max_freq")
+                else {
+                    continue;
+                };
+
+                clocks.push(Clock {
+                    identifier: Some(ClockIdentifier {
+                        domain: match (is_graphics, is_video) {
+                            (true, true) => ClockDomain::Gt as i32,
+                            (true, false) => ClockDomain::Graphics as i32,
+                            (false, true) => ClockDomain::VideoUnified as i32,
+                            (false, false) => continue,
+                        },
+                        index: i,
+                    }),
+                    current_frequency_mhz,
+                    max_frequency_mhz,
+                })
+            }
+        }
+        clocks
+    }
+
+    fn memory(&self) -> Vec<Memory> {
+        let query = drm_xe::DeviceQuery {
+            extensions: 0,
+            query: 1, // DRM_XE_DEVICE_QUERY_MEM_REGIONS
+            size: 0,
+            data: 0,
+            _reserved: [0; 2],
+        };
+
+        let Ok(mut query) = unsafe { rustix::ioctl::ioctl(self.render_node_fd.as_fd(), query) }
+            .inspect_err(|e| tracing::error!("failed to get xe memory regions: {e}"))
+        else {
+            return Vec::new();
+        };
+
+        // Now that query.size is populated, we can reserve a buffer
+        let mut bytes = Vec::new();
+        bytes.resize(query.size as usize, 0u8);
+        query.data = bytes.as_mut_ptr() as u64;
+
+        let Ok(_) = unsafe { rustix::ioctl::ioctl(self.render_node_fd.as_fd(), query) }
+            .inspect_err(|e| tracing::error!("failed to get xe memory regions: {e}"))
+        else {
+            return Vec::new();
+        };
+
+        // Now we transmute the bytes pointer to a drm_xe::QueryItem pointer
+        let Some(region_info) = core::ptr::NonNull::new(unsafe {
+            std::mem::transmute::<*const u8, *mut drm_xe::MemRegions>(bytes.as_ptr())
+        })
+        .map(|ptr| unsafe { ptr.as_ref() }) else {
+            return Vec::new();
+        };
+
+        // Now we extract the MemRegion values
+        let regions = unsafe {
+            region_info
+                .regions
+                .flex_ref(region_info.num_mem_regions as usize)
+        };
+
+        regions
+            .iter()
+            .map(|region| Memory {
+                r#type: if region.mem_class == 0 {
+                    MemoryType::System as i32
+                } else {
+                    MemoryType::Vram as i32
+                },
+                total_memory: region.total_size,
+                used_memory: region.used,
+            })
+            .collect()
+    }
+
+    fn power(&self) -> Option<Power> {
+        let Some(_hwmon_fd) = sysfs::first_hwmon_subdir_at(self.card_fd.as_fd(), "device/hwmon")
+        else {
+            return None;
+        };
+        None // I don't have an Arc GPU to figure out where the power file is located
+    }
+
+    fn thermals(&self) -> Vec<Thermal> {
+        let Some(_hwmon_fd) = sysfs::first_hwmon_subdir_at(self.card_fd.as_fd(), "device/hwmon")
+        else {
+            return Vec::new();
+        };
+        Vec::new() // Same as above
     }
 }
 
 impl super::Card for Card {
-    fn collect(&mut self, config: &super::Config) -> anyhow::Result<super::Gpu> {
-        todo!()
+    fn collect(&mut self, config: &Config) -> anyhow::Result<Gpu> {
+        let brand_name = self
+            .brand_name
+            .probe(|| Ok(String::from("todo")))
+            .cloned()
+            .unwrap_or_default();
+
+        let drivers = config
+            .drivers
+            .then(|| {
+                Some(Drivers {
+                    kernel: "i915".to_string(),
+                    ..Default::default()
+                })
+            })
+            .unwrap_or_default();
+
+        let engines = Vec::new();
+        let clocks = config.clocks.then(|| self.clocks()).unwrap_or_default();
+        let memory = config.memory.then(|| self.memory()).unwrap_or_default();
+        let power = config.power.then(|| self.power()).unwrap_or_default();
+        let thermals = config.thermals.then(|| self.thermals()).unwrap_or_default();
+
+        Ok(Gpu {
+            brand_name,
+            primary_node: self.primary_node.to_string_lossy().to_string(),
+            render_node: self.render_node.to_string_lossy().to_string(),
+            drivers,
+            engines,
+            clocks,
+            memory,
+            power,
+            thermals,
+            processes: Vec::new(),
+        })
     }
     fn resolve(
         &mut self,
-        staging: &crate::collector::staging::Staging,
-        output: super::Gpu,
-    ) -> anyhow::Result<super::Gpu> {
+        _staging: &crate::collector::staging::Staging,
+        _output: Gpu,
+    ) -> anyhow::Result<Gpu> {
         todo!()
     }
 
     fn primary_node(&self) -> String {
         todo!()
+    }
+}
+
+mod drm_xe {
+    use crate::collector::helpers::{ioctl::*, *};
+    use crate::{_ioc, _iowr};
+
+    /// Helper for a read-write DRM ioctl command
+    macro_rules! drm_iowr {
+        ($nr:expr, $ty:ty) => {
+            _iowr!(DRM_IOCTL_BASE, $nr, $ty)
+        };
+    }
+
+    /// The ioctl directory (drm)
+    const DRM_IOCTL_BASE: u32 = 'd' as u32;
+
+    /// The offset at which driver-specific drm commands begin
+    const DRM_COMMAND_BASE: u32 = 0x40;
+
+    /// The offset of the device query command
+    const DRM_XE_DEVICE_QUERY: u32 = 0x00;
+
+    /// The query command for drm ioctl
+    const DRM_IOCTL_XE_DEVICE_QUERY: u32 =
+        drm_iowr!(DRM_COMMAND_BASE + DRM_XE_DEVICE_QUERY, DeviceQuery);
+
+    /// struct drm_xe_device_query (linux/include/uapi/drm/xe_drm.h)
+    /// Holds a list of device queries
+    #[repr(C)]
+    #[derive(Debug, Clone)]
+    pub struct DeviceQuery {
+        pub extensions: u64,
+        pub query: u32,
+        pub size: u32,
+        pub data: u64,
+        pub _reserved: [u64; 2],
+    }
+
+    unsafe impl rustix::ioctl::Ioctl for DeviceQuery {
+        type Output = Self;
+        const IS_MUTATING: bool = true;
+
+        fn opcode(&self) -> rustix::ioctl::Opcode {
+            DRM_IOCTL_XE_DEVICE_QUERY
+        }
+
+        fn as_ptr(&mut self) -> *mut std::ffi::c_void {
+            self as *mut _ as *mut std::ffi::c_void
+        }
+
+        unsafe fn output_from_ptr(
+            _: rustix::ioctl::IoctlOutput,
+            extract_ptr: *mut std::ffi::c_void,
+        ) -> rustix::io::Result<Self::Output> {
+            let casted_ptr = core::ptr::NonNull::new(extract_ptr as *mut _ as *mut Self)
+                .map(|ptr| unsafe { ptr.as_ref() })
+                .ok_or(rustix::io::Errno::FAULT)?;
+
+            Ok(casted_ptr.clone())
+        }
+    }
+
+    /// struct drm_xe_device_query_mem_regions (linux/include/uapi/drm/xe_drm.h)
+    /// Holds memory region information for the device
+    #[repr(C)]
+    pub struct MemRegions {
+        pub num_mem_regions: u32,
+        pub _pad: u32,
+        pub regions: fam::FAM<MemRegion>,
+    }
+
+    /// struct drm_xe_mem_region (linux/include/uapi/drm/xe_drm.h)
+    /// Holds memory region information
+    #[repr(C)]
+    pub struct MemRegion {
+        pub mem_class: u16,
+        pub instance: u16,
+        pub min_page_size: u32,
+        pub total_size: u64,
+        pub used: u64,
+        pub cpu_visible_size: u64,
+        pub cpu_visible_used: u64,
+        pub _reserved: [u64; 6],
     }
 }
