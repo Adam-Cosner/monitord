14b682b5a9054bcf5e10c335df2ca8ee828b8e35
Adam Cosner <adam.cosner@proton.me>
2026-09-14 21:02:27 -0400

    build: restructured build.rs file and added serde macros

--- build.rs
+++ build.rs
@@ -4,28 +4,39 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    #[cfg(feature = "metrics")]
+#[allow(unused)]
+fn daemon_protos() -> Result<(), Box<dyn std::error::Error>> {
     tonic_prost_build::configure()
         .build_server(true)
         .build_client(false)
-        .compile_protos(
-            &[
-                "proto/metrics/v1/metrics.proto",
-                "proto/metrics/v1/cpu.proto",
-                "proto/metrics/v1/gpu.proto",
-                "proto/metrics/v1/memory.proto",
-                "proto/metrics/v1/network.proto",
-                "proto/metrics/v1/process.proto",
-                "proto/metrics/v1/storage.proto",
-            ],
-            &["proto/"],
-        )?;
-
-    #[cfg(feature = "daemon")]
-    tonic_prost_build::configure()
-        .build_server(true)
-        .build_client(false)
+        .type_attribute(
+            ".metrics.v1.cpu.Config",
+            "#[derive(serde::Deserialize, serde::Serialize)]",
+        )
+        .type_attribute(
+            ".metrics.v1.gpu.Config",
+            "#[derive(serde::Deserialize, serde::Serialize)]",
+        )
+        .type_attribute(
+            ".metrics.v1.memory.Config",
+            "#[derive(serde::Deserialize, serde::Serialize)]",
+        )
+        .type_attribute(
+            ".metrics.v1.network.Config",
+            "#[derive(serde::Deserialize, serde::Serialize)]",
+        )
+        .type_attribute(
+            ".metrics.v1.process.Config",
+            "#[derive(serde::Deserialize, serde::Serialize)]",
+        )
+        .type_attribute(
+            ".metrics.v1.storage.Config",
+            "#[derive(serde::Deserialize, serde::Serialize)]",
+        )
+        .type_attribute(
+            ".metrics.v1.Config",
+            "#[derive(serde::Deserialize, serde::Serialize)]",
+        )
         .compile_protos(
             &[
                 "proto/service/v1/service.proto",
@@ -33,12 +44,42 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             ],
             &["proto/"],
         )?;
+    Ok(())
+}
 
-    #[cfg(feature = "control")]
+#[allow(unused)]
+fn client_protos() -> Result<(), Box<dyn std::error::Error>> {
+    tonic_prost_build::configure()
+        .build_server(false)
+        .build_client(true)
+        .compile_protos(
+            &[
+                "proto/metrics/v1/metrics.proto",
+                "proto/service/v1/service.proto",
+            ],
+            &["proto/"],
+        )?;
+    Ok(())
+}
+
+#[allow(unused)]
+fn control_protos() -> Result<(), Box<dyn std::error::Error>> {
     tonic_prost_build::configure()
         .build_server(false)
         .build_client(true)
         .compile_protos(&["proto/control/v1/control.proto"], &["proto/"])?;
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    #[cfg(all(feature = "daemon", not(feature = "control"), not(feature = "client")))]
+    daemon_protos()?;
+
+    #[cfg(all(not(feature = "daemon"), feature = "control", not(feature = "client")))]
+    control_protos()?;
+
+    #[cfg(all(not(feature = "daemon"), not(feature = "control"), feature = "client"))]
+    client_protos()?;
 
     Ok(())
 }
